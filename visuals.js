5e7a350c115b4cd1357007cff8bcad2a61a3c6cb
Elliott Bunnell <elliottjbunnell@gmail.com>
2024-06-24 20:53:09 -0400

    Started with more three js, issue with require()

--- visuals.js
+++ visuals.js
@@ -10,24 +10,32 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( width, height );
 document.body.appendChild( renderer.domElement );
 
+const fs = require("fs");
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+// const directoryPath = '/path/to/your/directory';
+
+// // Use fs.readdirSync to read the contents of the directory synchronously
+// const fileList = fs.readdirSync(directoryPath);
+
+// console.log('Files and folders in the directory:', fileList);
+
+var map = new THREE.TextureLoader().load( "images/photos/12 elk 1.JPG" );
+var material = new THREE.MeshBasicMaterial( { map: map, color: 0xffffff } );
+var geometry = new THREE.BoxGeometry(10, 10, 0.1);
+var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-
+cube.rotation.y = -0.9;
 camera.position.z = 5;
+cube.position.x = 10;
 
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
 
 $(document).on("keydown", function(event) {
     if (event.key === "ArrowUp")
-        cube.position.y += 1;
+        camera.position.z += 1;
     else if (event.key === "ArrowDown")
-        cube.position.y -= 1;
+        camera.position.z -= 1;
 })
