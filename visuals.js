44fda1b6352406099d55cd8643c2669d106d79df
Elliott Bunnell <elliottjbunnell@gmail.com>
2024-06-25 22:09:58 -0400

    Worked more on visuals

--- visuals.js
+++ visuals.js
@@ -1,29 +1,37 @@
-import * as THREE from "three";
+import * as three from "three";
 
 const width = window.innerWidth * 0.8;
 const height = 400;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+const scene = new three.Scene();
+scene.background = new three.Color(0x3C3744);
+// scene.fog = new three.FogExp2(0xB4C5E4, 0.025);
+const camera = new three.PerspectiveCamera( 75, width / height, 0.1, 1000 );
 
-let canvas = document.getElementById("dolleyCanvas");
-const renderer = new THREE.WebGLRenderer({antialias: true});
+
+let canvas = document.getElementById("dolly-camera");
+const renderer = new three.WebGLRenderer({canvas: canvas, antialias: true});
 renderer.setSize( width, height );
 
-document.body.appendChild( renderer.domElement );
+const geometry = new three.BoxGeometry( 100, 0.1, 100 );
+const material = new three.MeshBasicMaterial( { color: 0x000000 } );
+const floor = new three.Mesh( geometry, material );
+floor.position.y = -5;
+floor.position.z = -30;
+scene.add( floor );
 
 const numImages = 10;
 
 let images = [];
 
 for (let i = 0; i < numImages; i++) {
-    let map = new THREE.TextureLoader().load(`images/photos/${i}.JPG`);
-    map.colorSpace = THREE.SRGBColorSpace;
-    let material = new THREE.MeshBasicMaterial( { map: map} );
-    let geometry = new THREE.BoxGeometry(13.3333333333, 10, 0.001);
-    images[i] = new THREE.Mesh( geometry, material );
+    let map = new three.TextureLoader().load(`images/photos/${i}.JPG`);
+    map.colorSpace = three.SRGBColorSpace;
+    let material = new three.MeshBasicMaterial( { map: map} );
+    let geometry = new three.BoxGeometry(13.3333333333, 10, 0.001);
+    images[i] = new three.Mesh( geometry, material );
     images[i].rotation.y = i % 2 === 0 ? -Math.PI / 12 : Math.PI / 12;
-    images[i].position.x = i % 2 === 0 ? 15 : -15;
+    images[i].position.x = i % 2 === 0 ? 7.5 : -7.5;
     images[i].position.z = -i * 10 - 10;
     scene.add(images[i]);
 }
@@ -35,11 +43,15 @@ renderer.setAnimationLoop( animate );
 
 $(document).on("keydown", function(event) {
     if (event.key === "ArrowUp")
-        camera.position.z -= 1;
+        camera.position.z -= .25;
     else if (event.key === "ArrowDown")
         camera.position.z += 1;
     else if (event.key === "ArrowLeft")
         camera.position.x -= 1;
     else if (event.key === "ArrowRight")
         camera.position.x += 1;
+    else if (event.key === "8")
+        camera.position.y += 1;
+    else if (event.key === "2")
+        camera.position.y -= 1;
 })
