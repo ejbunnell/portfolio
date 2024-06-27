fb1869b4e61d2a27f899ffa55b5546834e92d1a4
Elliott Bunnell <elliottjbunnell@gmail.com>
2024-06-26 21:00:59 -0400

    Fixed dolley added movable nav bar

--- script.js
+++ script.js
@@ -2,20 +2,45 @@ $(function() {
 
     let menuPos = 0;
 
+    $("#menu-icons").hide();
+
     $("#menu-icon").click( function() {
         if (menuPos === 0) {
-            $("#menu-icons").removeClass("hide");
-            $("#menu-icons").addClass("show");
-            $("#navbar").css("background-color", "#28242D");
+            $("#menu-icons").fadeIn(1500);
+            $("#navbar").removeClass("hide");
+            $("#navbar").addClass("show");
             $("#menu-icon").attr("src", "images/menuDark.png");
             menuPos = 1;
         }
         else if (menuPos === 1) {
-            $("#menu-icons").removeClass("show");
-            $("#menu-icons").addClass("hide");
-            $("#navbar").css("background-color", "#3C3744");
+            $("#menu-icons").fadeOut(100);
+            $("#navbar").removeClass("show");
+            $("#navbar").addClass("hide");
             $("#menu-icon").attr("src", "images/menuLight.png");
             menuPos = 0;
         }
     });
+
+    let cursorPos = {x: 0, y: 0};
+    $(document).on("mousemove", function(event) {
+        cursorPos.x = event.clientX - 30;
+        cursorPos.y = event.clientY - 30;
+    });
+
+    let moveNavbarLoop;
+    let moveNavbarDelay;
+    $("#navbar").on("mousedown", function() {
+        moveNavbarDelay = setTimeout(() => {
+            moveNavbarLoop = setInterval(() => {
+                $("#navbar").css("transform", `translateX(${(cursorPos.x / window.innerWidth) * 100}vw) translateY(${(cursorPos.y / window.innerHeight) * 100}vh)`)
+            }, 5);
+        }, 1000);
+    });
+
+    $(document).on("mouseup", function() {
+        clearInterval(moveNavbarLoop);
+        moveNavbarLoop = null;
+        clearTimeout(moveNavbarDelay);
+    });
+
 });
--- visuals.js
+++ visuals.js
@@ -24,12 +24,14 @@ const numImages = 10;
 
 let images = [];
 
+// Render images
+
 for (let i = 0; i < numImages; i++) {
     let map = new three.TextureLoader().load(`images/photos/${i}.JPG`);
     map.colorSpace = three.SRGBColorSpace;
     let material = new three.MeshBasicMaterial({map: map});
     let geometry = new three.BoxGeometry(13.3333333333, 10, 0.001);
-    images[i] = {photo: new three.Mesh( geometry, material), outOfWay: false, animateId : null};
+    images[i] = {photo: new three.Mesh( geometry, material), outOfWay: false};
     images[i].photo.rotation.y = i % 2 === 0 ? -Math.PI / 48 : Math.PI / 48;
     images[i].photo.position.x = i % 2 === 0 ? 3 : -3;
     images[i].photo.position.z = -i * 10 - 10;
@@ -42,34 +44,16 @@ function animate() {
 }
 
 function moveClosestPhoto() {
-    // let closestPhotoIndex = Math.round((camera.position.z + 10) / -10);
-    // if (closestPhotoIndex < 0) closestPhotoIndex = 0;
-    // else if (closestPhotoIndex > numImages - 1) closestPhotoIndex = numImages - 1;
-
-    // let dist = Math.abs(camera.position.z - images[closestPhotoIndex].photo.position.z);
-    // if (images[closestPhotoIndex].outOfWay === false) {
-    //     if (dist < 3) {
-    //         images[closestPhotoIndex].outOfWay = true;
-    //         animatePhotoAway(closestPhotoIndex);
-    //     }
-    // }
-    // else {
-    //     if (dist < 5) {
-    //         images[closestPhotoIndex].outOfWay = false;
-    //         animatePhotoBack(closestPhotoIndex);
-    //     }
-    // }
-    
+    const camZ = camera.position.z;
     for (let i = 0; i < numImages; i++) {
-        if (Math.abs(Math.abs(camera.position.z)- Math.abs(images[i].photo.position.z)) < 8) {
+        const photoZ = images[i].photo.position.z;
+        if ((camZ > photoZ && Math.abs(camZ - photoZ) < 3) || (camZ <= photoZ && Math.abs(camZ - photoZ) < 14)) {
             if (images[i].outOfWay === false) {
-                console.log(`Beginning of photo ${i} away`);
                 animatePhotoAway(i);
             }
         }
         else {
             if (images[i].outOfWay === true) {
-                console.log(`Beginning of photo ${i} back`);
                 animatePhotoBack(i);
             }
         }
@@ -77,41 +61,38 @@ function moveClosestPhoto() {
 }
 
 function animatePhotoAway(photoIndex) {
-    
     const dPI = photoIndex % 2 === 0 ? -Math.PI : Math.PI;
-    images[photoIndex].animateId = setInterval(() => {
-        images[photoIndex].photo.rotation.y += dPI / 48;
+    if (images[photoIndex].photo.rotation.y !== dPI / 48) return;
+    images[photoIndex].outOfWay = true;
+    const animateId = setInterval(() => {
+        images[photoIndex].photo.rotation.y += dPI / 192;
         if (Math.abs(images[photoIndex].photo.rotation.y) + 0.1 >= Math.PI / 2) {
-            images[photoIndex].outOfWay = true;
-            console.log(`End of photo ${photoIndex} away`);
             images[photoIndex].photo.rotation.y = dPI / 2;
-            clearInterval(images[photoIndex].animateId);
-            images[photoIndex].animateId = null;
+            clearInterval(animateId);
         }
-    }, 35)
+    }, 5)
 }
 
 function animatePhotoBack(photoIndex) {
     const dPI = photoIndex % 2 === 0 ? -Math.PI : Math.PI;
-    images[photoIndex].animateId = setInterval(() => {
-        images[photoIndex].photo.rotation.y -= dPI / 48;
+    if (images[photoIndex].photo.rotation.y !== dPI / 2) return;
+    images[photoIndex].outOfWay = false;
+    const animateId = setInterval(() => {
+        images[photoIndex].photo.rotation.y -= dPI / 192;
         if (Math.abs(images[photoIndex].photo.rotation.y) - 0.1 <= Math.PI / 48) {
-            images[photoIndex].outOfWay = false;
-            console.log(`End of photo ${photoIndex} back`);
             images[photoIndex].photo.rotation.y = dPI / 48;
-            clearInterval(images[photoIndex].animateId);
-            images[photoIndex].animateId = null;
+            clearInterval(animateId);
         }
-    }, 35)
+    }, 5)
 }
 
 renderer.setAnimationLoop( animate );
 
 $(document).on("keydown", function(event) {
     if (event.key === "ArrowUp")
-        camera.position.z -= .25;
+        camera.position.z -= 0.25;
     else if (event.key === "ArrowDown")
-        camera.position.z += 1;
+        camera.position.z += 0.25;
     else if (event.key === "ArrowLeft")
         camera.position.x -= 1;
     else if (event.key === "ArrowRight")
@@ -120,4 +101,8 @@ $(document).on("keydown", function(event) {
         camera.position.y += 1;
     else if (event.key === "2")
         camera.position.y -= 1;
+    else if (event.key === "7")
+        camera.rotation.x += .1;
+    else if (event.key === "1")
+        camera.rotation.x -= .1;
 })
