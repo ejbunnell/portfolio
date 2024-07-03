6048c785bd21c47cbb314246d12812ece96bddae
Elliott Bunnell <elliottjbunnell@gmail.com>
2024-07-03 01:48:27 -0400

    Pretty much finished

--- 5contact/contact.js
+++ 5contact/contact.js
@@ -1,3 +1,17 @@
 $(function() {
-    
+    emailjs.init({
+        publicKey: "j4SZx1lNSK6n6qv6H",
+    });
+    document.getElementById("contact-form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(() => {
+                $("#name").val("");
+                $("#email").val("");
+                $("#message").val("");
+                alert("Your message was succesfully sent!");
+            }, (error) => {
+                console.log(error);
+            });
+    });
 });
